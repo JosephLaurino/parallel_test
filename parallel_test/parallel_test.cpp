c8d6a520191d67041138ee36fb6d66759697cc7b
Joseph Laurino <joseph.laurino@gmail.com>
2019-09-06 14:42:21 -0700

    testing C++17 parallel algo

--- parallel_test/parallel_test.cpp
+++ parallel_test/parallel_test.cpp
@@ -2,20 +2,69 @@
 //
 
 #include "pch.h"
-#include <iostream>
+// compile with:
+//  debug: cl /EHsc /W4 /WX /std:c++latest /Fedebug /MDd .\program.cpp
+//  release: cl /EHsc /W4 /WX /std:c++latest /Ferelease /MD /O2 .\program.cpp
+#include <stddef.h>
+#include <stdio.h>
+#include <algorithm>
+#include <chrono>
+#include <random>
+#include <ratio>
+#include <vector>
+#include <execution>   // for the execution policy
 
-int main()
-{
-    std::cout << "Hello World!\n"; 
+using std::chrono::duration;
+using std::chrono::duration_cast;
+using std::chrono::high_resolution_clock;
+using std::milli;
+using std::random_device;
+using std::sort;
+using std::vector;
+
+const size_t testSize = 50'000'000;
+const int iterationCount = 5;
+
+void print_results(const char *const tag, const vector<double>& sorted,
+	high_resolution_clock::time_point startTime,
+	high_resolution_clock::time_point endTime) {
+	printf("%s: Lowest: %g Highest: %g Time: %fms\n", tag, sorted.front(),
+		sorted.back(),
+		duration_cast<duration<double, milli>>(endTime - startTime).count());
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+int main() {
+	random_device rd;
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+	// generate some random doubles:
+	printf("Testing with %zu doubles...\n", testSize);
+	vector<double> doubles(testSize);
+	for (auto& d : doubles) {
+		d = static_cast<double>(rd());
+	}
+
+// #define SERIAL
+#ifdef SERIAL 
+	// time how long it takes to sort them:
+	for (int i = 0; i < iterationCount; ++i)
+	{
+		vector<double> sorted(doubles);
+		const auto startTime = high_resolution_clock::now();
+		sort(sorted.begin(), sorted.end());
+		const auto endTime = high_resolution_clock::now();
+		print_results("Serial", sorted, startTime, endTime);
+	}
+#else
+
+	for (int i = 0; i < iterationCount; ++i)
+	{
+		vector<double> sorted(doubles);
+		const auto startTime = high_resolution_clock::now();
+		// same sort call as above, but with par_unseq:
+		sort(std::execution::par_unseq, sorted.begin(), sorted.end());
+		const auto endTime = high_resolution_clock::now();
+		// in our output, note that these are the parallel results:
+		print_results("Parallel", sorted, startTime, endTime);
+	}
+#endif
+}
